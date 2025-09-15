c80f2e431b0dd05ab1c15e352318be4cf500cba3
David Gebhart <davidgebhart@MacBookPro.attlocal.net>
2025-09-15 12:39:25 -0500

    Enable Waitress logging for clarity

--- webserver.py
+++ webserver.py
@@ -211,4 +211,6 @@ def home():
 # Run the app
 if __name__ == "__main__":
     from waitress import serve
+    import logging
+    logging.basicConfig(level=logging.INFO)
     serve(app, host="0.0.0.0", port=5000)
